28feeadcad97b13ad6ed97c61bbb7cb4bc10b571
Front-end-4 <waggner.dev@gmail.com>
2025-07-30 09:56:00 -0300

    Update

--- resources/views/client/blades/index.blade.php
+++ resources/views/client/blades/index.blade.php
@@ -24,4 +24,46 @@
         </div>
     @endforeach
 </section>
+
+<section id="topics">
+    <div class="col-12 topics">
+        <div class="row text-white text-center">
+            <div class="col-md-4 grey-background p-4 d-flex justify-content-between align-items-center">
+                <div class="mb-3">
+                    <i class="bi bi-stars" style="font-size: 2rem;"></i>
+                </div>
+                <div class="description text-start col-10">
+                    <h5 class="montserrat-bold font-25">Experiência</h5>
+                    <p class="mb-0 montserrat-regular font-15">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-md-4 bg-secondary p-4 d-flex justify-content-between align-items-center">
+                <div class="mb-3">
+                    <i class="bi bi-patch-check" style="font-size: 2rem;"></i>
+                </div>
+                <div class="description text-start col-10">                
+                    <h5 class="montserrat-bold font-25">Qualidade</h5>
+                    <p class="mb-0 montserrat-regular font-15">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-md-4 bg-dark p-4 d-flex justify-content-between align-items-center">
+                <div class="mb-3">
+                    <i class="bi bi-speedometer2" style="font-size: 2rem;"></i>
+                </div>
+                <div class="description text-start col-10">                    
+                    <h5 class="montserrat-bold font-25">Velocidade</h5>
+                    <p class="mb-0 montserrat-regular font-15">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
