bf1cf06d1794d1a4005a25e8077a35c2b052236b
Front-end-4 <waggner.dev@gmail.com>
2025-08-05 21:42:01 -0300

    Update HomePageController.php

--- app/Http/Controllers/Client/HomePageController.php
+++ app/Http/Controllers/Client/HomePageController.php
@@ -21,7 +21,17 @@ class HomePageController extends Controller
         $slides = Slide::active()->sorting()->get();
         $topics = Topic::active()->sorting()->get();
         $partners = Partner::active()->sorting()->get();
-        $plans = Plan::active()->sorting()->get();
+        $plans = Plan::select(
+        'plans.plan_category',
+        'plans.title',
+        'plans.subtitle',
+        'plans.bandwidth_limit',
+        'plans.bandwidth_unit',
+        'plans.description',
+        'plans.price',
+        'plans.active',
+        'plans.sorting'
+        )->active()->sorting()->get();
         $planCategories = PlanCategory::whereHas('plans')->active()->sorting()->get();
         $about = About::active()->first();
         $planSection = PlanSection::first();
@@ -31,7 +41,19 @@ class HomePageController extends Controller
 
     public function getPlansByCategory($id)
     {
-        $plans = Plan::where('plan_category', $id)->active()->sorting()->get();
+        $plans = Plan::select(
+        'plans.plan_category',
+        'plans.title',
+        'plans.subtitle',
+        'plans.bandwidth_limit',
+        'plans.bandwidth_unit',
+        'plans.description',
+        'plans.price',
+        'plans.active',
+        'plans.sorting'
+        )->where('plan_category', $id)
+        ->active()->sorting()->get();
+
         $html = view('client.blades.ajax.plan', compact('plans'))->render();
 
         return response()->json(['html' => $html]);
